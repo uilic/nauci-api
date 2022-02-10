30b56bee66dadc5479aa68e39278103f205fc83e
uilic <uros.ilic@fundingoptions.com>
2022-02-11 00:02:10 +0100

    update deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-sudo mkdir testdir
-
+sudo git pull origin main
+sudo pip3 install -r requirements.txt
+sudo mkdir testdirrrrrrr
+sudo systemctl restart gunicorn
+sudo systemctl restart nginx
