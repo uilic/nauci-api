09924b4a97b9c41df889b86a662b1a8be0728209
uilic <uros.ilic@fundingoptions.com>
2022-02-21 17:43:53 +0100

    remove logout from views

--- nauci_service/apps/accounts/urls.py
+++ nauci_service/apps/accounts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import RegisterAPI, LoginAPI, LogoutAPI, UserAPI
+from .views import RegisterAPI, LoginAPI, UserAPI
 
 urlpatterns = [
     path("register", RegisterAPI.as_view()),
     path("login", LoginAPI.as_view()),
-    path("logout", LogoutAPI.as_view()),
     path("user", UserAPI.as_view()),
 ]
